b3adf0b9138052bbdd456eb72e465d98418971c8
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-10-01 11:33:33 +0530

    1ST COMMIT

---
+++ pass 1- 12345.c
@@ -0,0 +1,15 @@
+//
+// Created by USER on 10/1/2023.
+//
+#include<stdio.h>
+int main(){
+int n;
+    printf("enter number of times you want to print :\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        printf("pass %d -",i);
+    for(int j=1;j<=5;j++)
+    printf("%d",j);
+    }
+    printf("\n");
+}
