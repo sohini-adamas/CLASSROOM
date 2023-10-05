89ae409c19f658292a6179301695676abc79acfa
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-10-05 17:16:43 +0530

    1ST COMMIT

---
+++ Pyramid.c
@@ -0,0 +1,21 @@
+//
+// Created by USER on 10/5/2023.
+//
+
+#include <stdio.h>
+int main(){
+    int i,j,rows,k,m;
+    printf("Enter rows:");
+    scanf("%d",&rows);
+
+    for(int i=1;i<=rows;i++) {
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (k = 1; k <= (i); k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+}
