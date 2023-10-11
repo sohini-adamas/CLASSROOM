fbefc24ffc257f34b0f4fbb5274e8ee3075a3f14
Sohini <145785887+sohini-adamas@users.noreply.github.com>
2023-10-11 16:47:32 +0530

    1ST COMMIT

---
+++ MY BESTO FRIENDO using function.c
@@ -0,0 +1,14 @@
+//
+// Created by USER on 10/11/2023.
+//
+#include <stdio.h>
+int bff(){
+    printf("MEENAL\n");
+    printf("MEENIE\n");
+    printf("POOH\n");
+    printf("MINI\n");
+    printf("AGRAWAL\n");
+}
+int main(){
+    bff();
+}
